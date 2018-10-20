d54cde3d6328b15e8b9fcb63131eda270a3b1e6e
jonas060831 <jonas_sulit2001@yahoo.com>
2018-10-20 07:53:13 -0700

    first commit

---
+++ Sources/App/Models/Post.swift
@@ -0,0 +1,39 @@
+import Vapor
+import FluentSQLite
+
+final class Post: Codable {
+    
+    var id: Int?
+    var Text: String?
+    var Image: String?
+    var Video: String?
+    var Location: String?
+    var Typeofpost: String
+    
+    
+    var Star: Int?
+    var Comment: String?
+    
+    
+    //Timestampable
+//    var createdAtKey: Date?
+//    var updatedAtKey: Date?
+    
+    
+    init(Text: String?, Image: String?, Video: String?, Location: String?, Typeofpost: String){
+        
+        self.Text = Text
+        self.Image = Image
+        self.Video = Video
+        self.Location = Location
+        self.Typeofpost = Typeofpost
+
+    }
+}
+
+extension Post: SQLiteModel {}
+
+extension Post: Migration {}
+
+extension Post: Content {}
+
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,19 +2,45 @@ import Vapor
 
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
-    // Basic "It works" example
-    router.get { req in
-        return "It works!"
-    }
+//    // Basic "It works" example
+//    router.get { req in
+//        return "It works!"
+//    }
+//
+//    // Basic "Hello, world!" example
+//    router.get("hello") { req in
+//        return "Hello, world!"
+//    }
+//    router.get("hello","vapor") { req in
+//        return "Hello Vapor!"
+//    }
+//    router.get("hello", String.parameter) { req -> String in
+//        //2
+//        let name = try req.parameters.next(String.self)
+//        // 3
+//        return "Hello, \(name)!"
+//    }
+//    //unchange
+//    router.post(InfoData.self, at: "info") { req, data -> String in
+//        return "Hello \(data.name)!"
+//    }
+//    //changed
+//    router.post(InfoData.self, at: "info2") { req, data -> InfoResponse in
+//        return InfoResponse(request: data)
+//    }
     
-    // Basic "Hello, world!" example
-    router.get("hello") { req in
-        return "Hello, world!"
+    router.post("api", "posts") { req -> Future<Post> in
+        return try req.content.decode(Post.self).flatMap(to: Post.self) { post in
+            return post.save(on: req)
+        }
     }
 
-    // Example of configuring a controller
-    let todoController = TodoController()
-    router.get("todos", use: todoController.index)
-    router.post("todos", use: todoController.create)
-    router.delete("todos", Todo.parameter, use: todoController.delete)
+}
+
+struct InfoData: Content {
+    let name: String
+}
+
+struct InfoResponse: Content {
+    let request: InfoData
 }
