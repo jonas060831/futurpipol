71bf81bc183ef35f7f63247cc1f9959cf637f41f
jonas060831 <jonas_sulit2001@yahoo.com>
2018-10-26 17:00:55 -0700

    added navigation bar

--- Sources/App/Controllers/WebsiteController.swift
+++ Sources/App/Controllers/WebsiteController.swift
@@ -5,6 +5,8 @@ struct WebsiteController: RouteCollection {
     
     func boot(router: Router) throws {
         router.get(use: indexHandler)
+        router.get("login", use: loginHandler)
+        router.get("user", use: userHandler)
     }
     
     func indexHandler(_ req: Request) throws -> Future<View> {
@@ -17,9 +19,33 @@ struct WebsiteController: RouteCollection {
             return try req.view().render("index", context)
         }
     }
+    
+    func userHandler(_ req: Request) throws -> Future<View> {
+        return User.query(on: req).all().flatMap(to: View.self) { user in
+            let userData = user.isEmpty ? nil : user
+            let context = UserContext(
+                title: "User",
+                user: userData
+            )
+            return try req.view().render("user", context)
+        }
+    }
+    
+    func loginHandler(_ req: Request) throws -> Future<View> {
+        let context = LoginContext(title: "Log In")
+        return try req.view().render("login", context)
+    }
 }
 
 struct IndexContext: Encodable {
     let title: String
     let posts: [Post]?
+}
+
+struct LoginContext: Encodable {
+    let title: String
+}
+struct UserContext: Encodable {
+    let title: String
+    let user: [User]?
 }
